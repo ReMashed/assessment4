2e300e03fbc22372acb70d56734f4ffd82657fa6
Kevin L <ReMashed@users.noreply.github.com>
2020-05-25 21:34:50 +1000

    Movement (.w/ raycast) + collision detection + boost Items
    - Change W and arrow key to jump instead of space
    - Player uses raycast (not being utilised properly yet)
    - Player can double jump only
    - Collision detection with items
    - One obstacle using background for testing purposes.

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -3,14 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class InputManager : MonoBehaviour
-{
+{   
     private Vector3 movement; //2D so ignore z axis
     private bool jump;
+    private RaycastHit2D hit; 
+    private bool canDoubleJump = false; 
 
     public float speed = 3f; 
     public Rigidbody2D rb; 
-    //Animations will probs go in seperate script later
-    public Animator animator; 
+    public Animator animator; //Animations will probs go in seperate script later
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -21,6 +23,11 @@ public class InputManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        hit = Physics2D.Raycast(transform.position, -Vector2.up, 10);
+        //Debug.Log(hit.distance + "distance from collider");
+        //Debug.DrawRay(transform.position, dwn*10, Color.red, 0.5f);
+        //Debug.Log("collider hit" + hit.collider);
+        //Debug.Log(rb.velocity);
         GetMovementInput();
         MoveObject(); 
         Rotate();
@@ -31,35 +38,47 @@ public class InputManager : MonoBehaviour
         //continuous values between -1, 1 
         //will need to clamp values for maintain values of 1 when diagonals occur
         movement.x = Input.GetAxis("Horizontal");
-        //movement.y = Input.GetAxis("Vertical");
+        jump = Input.GetButtonDown("Vertical"); //jump should be instant
         //clamping uneccessary if just horizontal movement
         //movement = Vector3.ClampMagnitude(movement, 1.0f);
         //Debug.Log(movement.sqrMagnitude); 
-        jump = Input.GetButtonDown("Jump");
+        //jump = Input.GetButtonDown("Jump");
     }
 
     void MoveObject(){
         //framerate independent movement (not affected by framerate)
         //transform is object script is attached to
-        //time.delta makes this framerate independent 
-        if (jump != true){
+        //time.delta makes this framerate independent
+                
+        if (!(jump) && movement.x != 0){
             transform.position += movement * speed * Time.deltaTime;
-            
-        }else if (jump && transform.position.y <= 2 ) {
-            //if player y is equal to ground level, jump is available.
-            //transform.Translate(Vector3.up * Time.deltaTime * 100);
-            //Debug.Log(transform.position.y);
+            animator.SetTrigger("Run"); //trigger running animation.
+        } else if (jump ) {
             animator.SetTrigger("Jump");
-            rb.velocity = new Vector3(0, 7, 0);
-        } 
+            //if touching ground. 
+            if (hit.distance < 0.6) {
+                rb.velocity = new Vector2(0, 7);
+                canDoubleJump = true; 
+            } 
+            else {
+                if (canDoubleJump) {
+                canDoubleJump = false; 
+                rb.velocity = new Vector2(0, 7);
+                 } 
+            }
+            
+        } else {
+            //when player stops moving set back to idle
+            //Debug.Log(hit.distance);
+            animator.SetTrigger("Idle");
+            }
     }
 
     void Rotate(){
         //player is moving right
         if (movement.x != 0)
             {
-                //Debug.Log(movement.x);
-                animator.SetTrigger("Run"); //trigger running animation. 
+                //Debug.Log(movement.x); 
                 if (movement.x > 0) //if player is going right (position)
                 {
                     //transform.LookAt(Vector3.right);
@@ -70,15 +89,7 @@ public class InputManager : MonoBehaviour
                 {   
                     //transform.LookAt(Vector3.left);
                     gameObject.transform.rotation = Quaternion.LookRotation(new Vector3(0,0,movement.x ),  Vector3.up);
-                }
-                    
-                
+                }     
             } 
-            else
-            {
-                //when player stops moving set back to idle
-                Debug.Log(movement.x);
-                animator.SetTrigger("Idle");
-            }
     }
 }
---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float health = 100; 
+    //public GameObject Obj;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        health -= Time.deltaTime;
+    }
+    //script is attached to player, so whatever it touches
+    void OnTriggerEnter2D(Collider2D other) { 
+        Debug.Log("touchOther");
+        if (other.gameObject.name == "HealthBoost")
+        {
+            Destroy(other.gameObject);
+            //If the GameObject has the same tag as specified, output this message in the console
+            Debug.Log("touch");
+            health += 20; 
+            Debug.Log(health + "health");
+
+
+        }
+        if (other.gameObject.name == "SpeedBoost") {
+            //gameObject.GetComponent<InputManager>().speed = gameObject.GetComponent<InputManager>().speed*2;
+            gameObject.GetComponent<InputManager>().rb.velocity = new Vector2(6,0);
+            Destroy(other.gameObject); 
+        }
+    }
+}
